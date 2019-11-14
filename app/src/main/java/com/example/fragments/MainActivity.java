99b45e6ed14d88b3ee22ccda3f8fe2382b249f53
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2019-11-14 14:32:07 +0100

    Commit alternar entre fragments

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -15,21 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.boton).setOnClickListener(new View.OnClickListener() {
+
+            FragmentUno f1 = new FragmentUno();
+            FragmentDos f2 = new FragmentDos();
+
             @Override
             public void onClick(View view) {
                 pulsado = !pulsado;
                 FragmentManager fm = getSupportFragmentManager();
                 FragmentTransaction ft = fm.beginTransaction();
-                FragmentUno f1 = new FragmentUno();
-                FragmentDos f2 = new FragmentDos();
+
                 if (pulsado){
-                    ft.replace(R.id.contenedor,f2);
+                    ft.add(R.id.contenedor,f2);
                 }else
                 {
-                    ft.replace(R.id.contenedor,f1);
+                    ft.add(R.id.contenedor,f1);
                 }
 
+                ft.addToBackStack(null);
+                //fm.popBackStack();
+                /*quita todas los fragments que se han creado al darle atras...vuelve al principio*/
                 ft.commit();
+                pulsado = !pulsado;
 
 
             }
